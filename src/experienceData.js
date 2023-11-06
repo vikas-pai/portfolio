b11870a030fe876d8501ee8d4889cc25eb442414
vikas-pai <paivikas1402@gmail.com>
2023-11-06 17:24:33 +0530

    portfolio website

---
+++ src/experienceData.js
@@ -0,0 +1,10 @@
+const experience = [
+  {
+    title: "StackIt",
+    image: "./images/rn-mockup",
+    description:
+      " An all-in-one website for onboarding clubs and managing their events. Provided students with a centralized platform to view,register and keep track of upcoming events. ",
+    link: "https://www.nowstackit.com/",
+  },
+];
+export default experience;
---
+++ src/projectData.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    title: "Event Central",
+    image: "./images/rn-mockup",
+    description:
+      " An all-in-one website for onboarding clubs and managing their events. Provided students with a centralized platform to view,register and keep track of upcoming events. ",
+    tools: ["Html", "Css", "NodeJs", "MySql", "ExpressJs"],
+    github: "https://github.com/Deelip7/react-node-ecommerce",
+  },
+  {
+    title: "Wordle 2.0",
+    image: "./images/tmdb-mockup",
+    description:
+      "Configurable singleplayer mode with adjustable word lengths and difficulty levels. Added multiplayer mode for real-time competition between two players.",
+    tools: ["Html", "TailWind Css", "NodeJs", "ExpressJs"],
+    github: "https://github.com/Deelip7/react-tmdb-app",
+  },
+  {
+    title: "Project Mate",
+    image: "./images/recipeApp-mockup",
+    description:
+      "A project management and issue tracking tool to enhance team collaboration and organization. Enabled teams to track the status and manage the lifecycle of bugs, tasks, and improvements within a project.",
+    tools: ["Html", "Css", "MongoDb", "NodeJs", "ExpressJs"],
+    github: "https://github.com/Deelip7/RecipesFinder",
+  },
+];
+
+export default projects;
